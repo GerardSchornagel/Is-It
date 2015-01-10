e81bb9141d732599993f270d134dfef86f71d6ce
Gerard Evert Schornagel <gschornagel@hotmail.com>
2015-01-10 18:01:10 +0100

    Initial creation of Solution File.

---
+++ Is-It/MainForm.vb
@@ -0,0 +1,13 @@
+﻿''' <summary>
+''' Description of form/class/etc.
+''' </summary>
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
